1d5c0206ced4bcb6aab63cb794819884f302905e
minpann <705450732@qq.com>
2016-08-27 19:40:29 +0800

    Initial commit

---
+++ taotao-manager-service/src/main/java/com/taotao/service/ItemService.java
@@ -0,0 +1,10 @@
+package com.taotao.service;
+
+import com.taotao.pojo.TbItem;
+
+/**
+ * Created by panmin on 16-8-27.
+ */
+public interface ItemService {
+    TbItem getItemById(long itemId);
+}
---
+++ taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -0,0 +1,27 @@
+package com.taotao.controller;
+
+import com.taotao.pojo.TbItem;
+import com.taotao.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by panmin on 16-8-27.
+ */
+@Controller
+public class ItemController {
+
+    @Autowired
+    private ItemService itemService;
+
+    @RequestMapping("/item/{itemId}")
+    @ResponseBody
+    public TbItem getItemById(@PathVariable Long itemId)
+    {
+        TbItem tbItem = itemService.getItemById(itemId);
+        return tbItem;
+    }
+}
