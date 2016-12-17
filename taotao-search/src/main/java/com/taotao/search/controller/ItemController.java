5aa02af8f4d954c394dc38a01311ee84f9bf4c01
minpann <705450732@qq.com>
2016-12-17 22:22:58 +0800

    search module create and init

--- taotao-search/src/main/java/com/taotao/search/controller/ItemController.java
+++ taotao-search/src/main/java/com/taotao/search/controller/ItemController.java
@@ -1,12 +1,11 @@
 package com.taotao.search.controller;
 
+import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.search.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.taotao.common.pojo.TaotaoResult;
-import com.taotao.search.service.ItemService;
 
 /**
  * 索引库维护
